9bcac40608e2de999938964607dfac8a13596c95
Ekenzy-101 <ekeneonyekaba@gmail.com>
2021-01-18 04:24:00 +0100

    Added the post likes page and functionality

---
+++ src/containers/post-likes/styles.ts
@@ -0,0 +1,22 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    background: theme.palette.background.paper,
+    minHeight: "100vh",
+    width: "100%",
+    maxWidth: "100vw",
+    paddingBottom: "3.2rem",
+  },
+  wrapper: {
+    width: "598px",
+    margin: "auto",
+    display: "block",
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+    },
+    [theme.breakpoints.up("sm")]: {
+      marginTop: "30px",
+    },
+  },
+}));
