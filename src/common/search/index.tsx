8787c2f8b94aa7801231922616e77e07a5d04d82
Ekenzy-101 <ekeneonyekaba@gmail.com>
2021-02-06 18:34:56 +0100

    Added search functionality for desktop view

--- src/common/search/index.tsx
+++ src/common/search/index.tsx
@@ -1,29 +1,182 @@
-import { InputBase } from "@material-ui/core";
-import { SearchOutlined } from "@material-ui/icons";
-import React from "react";
-import { useStyles } from "./styles";
+import { Avatar, InputBase, Typography } from "@material-ui/core";
+import { SearchOutlined, Cancel } from "@material-ui/icons";
+import { Autocomplete } from "@material-ui/lab";
+import React, { useEffect, useReducer, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
-interface Props {
-  fullWidth?: boolean;
-  borderRadius?: string;
+import { useStyles } from "./styles";
+import { getUsersBySearch } from "../../utils/services/userService";
+import { User } from "../../utils/types/user";
+import { LOADING_GIF_URL, PROFILE_PIC_URL } from "../../utils/constants/url";
+
+type State = {
+  users: User[];
+  status: "focused" | "none";
+  query: string;
+  loading: boolean;
+  hasFetchedBefore: boolean;
+};
+
+type Action =
+  | { type: "focus" }
+  | { type: "reset" }
+  | { type: "request" }
+  | { type: "success"; payload: User[] }
+  | { type: "blur" }
+  | { type: "handlechange"; payload: string };
+
+const initialState: State = {
+  users: [],
+  status: "none",
+  query: "",
+  loading: false,
+  hasFetchedBefore: false,
+};
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "reset":
+      return { ...state, query: "" };
+    case "request":
+      return { ...state, loading: true };
+    case "success":
+      return {
+        ...state,
+        loading: false,
+        users: action.payload,
+        hasFetchedBefore: true,
+      };
+    case "focus":
+      return { ...state, status: "focused" };
+    case "blur":
+      return { ...state, status: "none" };
+    case "handlechange":
+      return {
+        ...state,
+        query: action.payload,
+        status: "focused",
+        hasFetchedBefore: action.payload ? true : false,
+      };
+    default:
+      return state;
+  }
 }
 
-const CustomSearch: React.FC<Props> = ({ fullWidth, borderRadius }) => {
+const CustomSearch: React.FC = () => {
+  // State Hooks
+  const [{ query, status, loading, users }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const [value, setValue] = useState<null | User>(null);
+  const ref = useRef<NodeJS.Timeout | null>(null);
+
   // Other Hooks
-  const classes = useStyles();
+  const classes = useStyles({ status });
+
+  // Effect Hooks
+  useEffect(() => {
+    ref.current = setTimeout(async () => {
+      if (query) {
+        dispatch({ type: "request" });
+
+        const { data } = await getUsersBySearch(query.toLowerCase());
+
+        dispatch({ type: "success", payload: data as User[] });
+      }
+    }, 1000);
+  }, [query]);
+
+  // Event Handlers
+  const handleChange = (e: React.ChangeEvent<{}>, value: string) => {
+    console.log(value);
+    dispatch({ type: "handlechange", payload: value });
+    clearTimeout(ref.current!);
+  };
+
+  const handleBlur = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    setTimeout(() => {
+      dispatch({ type: "blur" });
+    }, 200);
+  };
+
+  const handleFocus = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    dispatch({ type: "focus" });
+  };
+
+  const handleReset = () => {
+    dispatch({ type: "reset" });
+  };
 
   // JSX
   return (
-    <InputBase
-      placeholder=" Search"
-      fullWidth={fullWidth}
-      className={classes.root}
-      style={{
-        borderRadius: borderRadius ? borderRadius : "4px",
-        width: fullWidth ? "100%" : "215px",
-      }}
-      startAdornment={<SearchOutlined className={classes.searchIcon} />}
-    />
+    <>
+      <Autocomplete
+        id="search-box"
+        clearOnBlur={false}
+        clearOnEscape
+        noOptionsText="No results found."
+        value={value}
+        onChange={(_, newValue) => {
+          setValue(newValue);
+        }}
+        inputValue={query}
+        onInputChange={handleChange}
+        ListboxProps={{ className: classes.listBox }}
+        options={users}
+        getOptionLabel={(option) => option.username!}
+        renderInput={(params) => (
+          <div className={classes.root} ref={params.InputProps.ref}>
+            <InputBase
+              placeholder="Search"
+              className={classes.inputBase}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              inputProps={{ className: classes.input, ...params.inputProps }}
+              startAdornment={<SearchOutlined className={classes.searchIcon} />}
+              endAdornment={
+                loading ? (
+                  <Avatar
+                    src={LOADING_GIF_URL}
+                    style={{ width: "1rem", height: "1rem" }}
+                  />
+                ) : status === "focused" ? (
+                  <Cancel
+                    onClick={handleReset}
+                    className={classes.cancel}
+                    focusable={undefined}
+                  />
+                ) : undefined
+              }
+            />
+          </div>
+        )}
+        renderOption={({ username, id, image_url, name }) => (
+          <>
+            <Link to={`/${username}/`} key={id} className={classes.link}>
+              <div className={classes.menuItem}>
+                <Avatar
+                  className={classes.avatar}
+                  src={image_url ? image_url : PROFILE_PIC_URL}
+                />
+                <div>
+                  <Typography className={classes.username}>
+                    <strong>{username}</strong>
+                  </Typography>
+                  <Typography className={classes.name} color="textSecondary">
+                    {name}
+                  </Typography>
+                </div>
+              </div>
+            </Link>
+          </>
+        )}
+      />
+    </>
   );
 };
 
--- src/common/search/styles.ts
+++ src/common/search/styles.ts
@@ -2,16 +2,69 @@ import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles((theme) => ({
   root: {
+    width: "350px",
+    display: "flex",
+    justifyContent: "center",
+  },
+  avatar: {
+    width: 38,
+    height: 38,
+    marginRight: 10,
+  },
+  cancel: {
+    fontSize: "1rem",
+    color: theme.palette.action.disabled,
+    cursor: "pointer",
+    zIndex: 4959,
+  },
+  input: {
+    fontSize: "0.9rem",
+  },
+  inputBase: (props: { status?: string }) => ({
     background: theme.palette.background.default,
     height: "1.75rem",
     width: "215px",
-    borderRadius: "3px",
+    borderRadius: "4px",
     border: "0.3px solid rgba(var(--b6a,219,219,219),1)",
     fontSize: "1rem",
-    paddingLeft: "3rem",
+    paddingLeft: props.status === "focused" ? "0.5rem" : "3rem",
+    paddingRight: "0.4rem",
+  }),
+  link: {
+    cursor: "pointer",
+    textDecoration: "none",
+    color: "inherit",
+    display: "block",
+  },
+  listBox: {
+    padding: 0,
+    margin: 0,
+    width: "100%",
+    maxHeight: 362,
+    overflow: "auto",
+    "& li": {
+      borderBottom: "1px solid rgba(var(--b6a,219,219,219),1)",
+      padding: 0,
+      margin: 0,
+    },
+  },
+  menuItem: {
+    width: "100%",
+    height: 60,
+    display: "flex",
+    alignItems: "center",
+    padding: "8px 14px",
+  },
+  name: {
+    fontSize: "0.9rem",
   },
   searchIcon: {
     fontSize: "1rem",
     color: theme.palette.action.disabled,
+    fontWeight: "bold",
+  },
+  username: {
+    color: "#262626",
+    fontSize: "0.9rem",
   },
 }));
